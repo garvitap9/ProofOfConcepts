83278c617afcf59af9ce23c2340817d8e0a07e88
garvita <garvita.pun9@gmail.com>
2025-02-19 18:57:47 +0530

    done

---
+++ ConstructorWithOverloading/ConstructorOverloading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorWithOverloading
+{
+    public class ConstructorOverloading
+    {
+        public static int Number1 {  get; set; }
+        public static int Number2 { get; set; }
+
+        public ConstructorOverloading(int numBer) 
+        { 
+            Number1 = numBer;
+        }
+
+        public ConstructorOverloading()
+        {
+            Number2 = 42;
+        }
+
+        public static void DisplayNumbers()
+        {
+            Console.WriteLine("Exploring the concept of Constructor and constructor overloading in a simple way");
+            Console.WriteLine("Number1: "+Number1);
+            Console.WriteLine("Number2: " + Number2);
+        }
+    }
+}
---
+++ ConstructorWithOverloading/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorWithOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorOverloading constructorOverloading1 = new ConstructorOverloading(99);
+            ConstructorOverloading constructorOverloading2 = new ConstructorOverloading();
+            ConstructorOverloading.DisplayNumbers();
+        }
+    }
+}
