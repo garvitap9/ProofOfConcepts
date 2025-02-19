5a22fe711800405ab965360d700ace581f1a7602
garvita <garvita.pun9@gmail.com>
2025-02-19 19:14:12 +0530

    chaining done

---
+++ ConstructorChaining/ConstructorChainingConcept.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    public class ConstructorChainingConcept
+    {
+        public ConstructorChainingConcept():this(98)
+        {
+            Console.WriteLine("Constructor with No argument!");
+        }
+        public ConstructorChainingConcept(int numBer) : this("Garvita")
+        {
+            Console.WriteLine("Constructor with int argument! Value: "+numBer);
+        }
+        public ConstructorChainingConcept(string nameStore)
+        {
+            Console.WriteLine("Constructor with string argument! Value: "+nameStore);
+        }
+
+    }
+}
---
+++ ConstructorChaining/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorChainingConcept constructorChainingConcept = new ConstructorChainingConcept();
+        }
+    }
+}
